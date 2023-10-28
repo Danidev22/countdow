f83dd0c40b113fd838f6715e1f8c878a3f7862d7
unknown <daniele.alves.oliveira.2@gmail.com>
2023-10-28 17:12:18 -0300

    teste

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import Title from './components/title';
-import Counter from './components/Counter';
+import Title from "./components/Title.jsx";
+import Counter from "./components/Counter.jsx";
 
-import anonovo from './assets/anonovo.jpg';
+import anonovo from "./assets/anonovo.jpg";
 
-import useCountdown from './hooks/useCountdown';
+import useCountdown from "./hooks/useCountdown.jsx";
 
-import './App.css';
+import "./App.css";
 
 
 function App() {
   const [day, hour, minute, second] = useCountdown("jan 1, 2024 00:00:00");
   
   return (
-    <div className='App' style={{backgroundImage: `url(${anonovo})` }}>
-      <div className='container'>
-        <Title title='Contagem regressiva para 2024!'/>
-        <div className='countdown-container'>
-          <Counter title='Dias' number={day} />
-          <Counter title='Horas' number={hour} />
-          <Counter title='Minutos' number={minute} />
-          <Counter title='Segundos' number={second} />
+    <div className="App" style={{backgroundImage: `url(${anonovo})` }}>
+      <div className="container">
+        <Title title="Contagem regressiva para 2024!"/>
+        <div className="countdown-container">
+          <Counter title="Dias" number={day} />
+          <Counter title="Horas" number={hour} />
+          <Counter title="Minutos" number={minute} />
+          <Counter title="Segundos" number={second} />
         </div>
       </div>
     </div>
